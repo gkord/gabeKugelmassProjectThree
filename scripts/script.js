a5cd698653f4fcfa09b7ba746950f913118141a2
Gabe Kugelmass <gkord@me.com>
2019-08-23 20:51:10 -0400

    friday eod commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,327 @@
-const wineApp = {};
+wineApp = {};
 
-// wineApp.images = {
-//     cabSauv: {
-//         img: ''
-//     },
-//     pinotNoir: {
-//         img: ''
-//     },
-//     sangiovese: {
-//         img: ''
-//     }
-// }
-
-wineApp.choices = {
-    happy: {
-        wine: [
-            'Champagne',
-            'Cabernet Sauvignon',
-            'Sangiovese'
+wineApp.wineChoice = {
+    red: {
+        reflective: [
+            {
+                varietal: 'Cabernet Sauvignon',
+                description: 'A robust red that is generally full-bodied, dry, and abundant in tannin. This wine pairs well with your reflective mood due to its complexity. There are many layers too unravel. Look for something from the Left Bank of Bordeaux, Napa Valley, California or Tuscany, Italy.'
+            },
+            { 
+                varietal: 'Burgundy Pinot Noir',
+                description: 'The crown jewel of red wines. Burgundy Pinot Noir comes in many styles from light and delicate to full-bodied and rich. Try something from the Cote-de-Nuits appellation and take some time to be present in the moment and savour the  notes of black currant, cherry, fresh red fruits, and earthy mushroom and spice.'
+            },
+            {
+                varietal: 'Chateauneuf du Pape',
+                description: 'A classic blend comprised of predominantly Grenache, with Syrah and Mourvedre making up the remainder. This wine from France’s Southern Rhone region offers great complexity and a nice departure from the norm. Full bodied and intense, this is a wine pairs perfectly with your contemplative mood and offers generous notes of blackberry, dark cherry, liquorice and spice.'
+            }  
+        ],
+        energetic: [
+            {
+                varietal: 'New World Pinot Noir',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Frappato',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Zinfandel',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        mellow: [
+            {
+                varietal: 'Merlot',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Tempranillo',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Sangiovese',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        silly: [
+            {
+                varietal: 'Zinfandel',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Alsacian Pinot Noir',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Aglianico',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        gloomy: [
+            {
+                varietal: 'Nebbiolo',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Valpolicella',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Gamay',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        celebratory: [
+            {
+                varietal: 'Cabernet Sauvignon',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Rioja',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Super Tuscan',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        indifferent: [
+            {
+                varietal: 'Merlot',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Sangiovese',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Cabernet Franc',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        angry: [
+            {
+                varietal: 'Shiraz',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Meritage',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Nero D\'Avola',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
         ]
     },
-    sad: {
-        wine: [
-            'Pinot Noir',
-            'Nebbiolo',
-            'Gamay'
+
+    white: {
+        reflective: [
+            {
+                varietal: 'Burgundy Chardonnay',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'German Riesling',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Australian Semillon',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        energetic: [
+            {
+                varietal: 'New Zealand Sauvignon Blanc',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Pinot Blanc',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Southern Rhone Blend',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        mellow: [
+            {
+                varietal: 'Unoaked Chardonnay',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Pinot Grigio',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Arneis',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        silly: [
+            {
+                varietal: 'Greco di Tufo',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Assyrtico',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Vermentino',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        gloomy: [
+            {
+                varietal: 'Chardonnay',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Pinot Gris',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Chateauneuf du Pape',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        celebratory: [
+            {
+                varietal: 'Chardonnay',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Viognier',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Moscato',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        indifferent: [
+            {
+                varietal: 'Pinot Grigio',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Sauvignon Blanc',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Dry Riesling',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
+        ],
+        angry: [
+            {
+                varietal: 'Albarinho',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Chenin Blanc',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                varietal: 'Grillo',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }  
         ]
     },
-    neutral: {
-        wine: [
-            'Merlot',
-            'Syrah',
-            'Valpolicella'
+
+    other: {
+        reflective: [
+            {
+                varietal: 'Franciacorta',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+        ],
+        energetic: [
+            {
+                varietal: 'Prosecco',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+        ],
+        mellow: [
+            {
+                varietal: 'Port',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+        ],
+        silly: [
+            {
+                varietal: 'Sparkling',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+        ],
+        gloomy: [
+            {
+                varietal: 'Champagne',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+        ],
+        celebratory: [
+            {
+                varietal: 'Champagne',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+        ],
+        indifferent: [
+            {
+                varietal: 'New World Sparkling',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+        ],
+        angry: [
+            {
+                varietal: 'Champagne',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
         ]
     }
 }
-
-$(function(){
 
     const getRandomNumberFromArray = function (array) {
         const randomNum = Math.floor(Math.random() * array.length);
         return array[randomNum];
     };
+
+    
+    wineApp.getWine = function(){
     //listen for submit of user choices
-    $('form').on('submit', function(event) {
+    $('.submitMood').on('click', function(event) {
         event.preventDefault();
     //create variable to store user mood choice
-    const userMood = $('input[name=mood]:checked').val()
-    console.log(userMood);
-    //select appropriate array from wine object based on user input
-    const wineArray = (wineApp.choices[userMood]);
-    console.log(wineArray);
-    //display results on the screen
-    $('.result').html(`<h2 class="choice">${wineArray.wine}</h2>`);
-    // disable submit button after entering
-    $(`input[type=submit]`).attr('disabled', 'disabled');
+    wineApp.userMood = $('input[name=mood]:checked').val()
     });
-    //reset the entire page when reset button is clicked
-    $('form').on('reset', function() {
-        //hard refresh page
-        location.reload(true);
+
+    $('.submitWine').on('click', function(event){
+        event.preventDefault();
+    //create variable to store user wine choice
+        const userWine = $('input[name=drink]:checked').val()
+    //select appropriate object from wineChoices object based on user choice
+        const wineObject = wineApp.wineChoice[userWine];
+        console.log(wineObject);
+        const wineArray = wineObject[wineApp.userMood];
+        console.log(wineObject);
+        const wineSelection = getRandomNumberFromArray(wineArray);
+        //display results on the screen
+        $('.result').html(`<h2 class="choice">${wineSelection.varietal}</h2>`);
+        $('.result').append(`<p>${wineSelection.description}</p>`)
+        $('.result').append(`<button class="reset">Reset</button>`);
+        $('.result').on('click', '.reset', function () {
+            //hard refreshes page
+            location.reload(true);
+        });
+    })
+}
+    
+    $(function () {
+        $(this).scrollTop(0);
+        wineApp.getWine();
+        $('a').smoothScroll({
+            speed: 800
+        });
     });
-});
